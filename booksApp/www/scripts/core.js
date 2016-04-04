9d7c0098b5eee2ce6c2dff8bd77c5fd368b8e210
gulhermecoelho <guilhermecoelho2@gmail.com>
2016-04-04 14:12:26 -0300

    create form

---
+++ booksApp/www/scripts/core.js
@@ -0,0 +1,35 @@
+﻿//common functions for all system
+
+"use strict";
+$(document).ready(function () {
+
+    createMenu();
+
+});
+
+//create menu itens
+function createMenu() {
+    var righthide = '<ul class="right hide-on-med-and-down">';
+    var slideOut = '<ul id="slide-out" class="side-nav">';
+    var button = '<a href="#" data-activates="slide-out" class="button-collapse"><i class="mdi-navigation-menu"></i></a>';
+
+    //menu itens
+    var index = '<li><a href="index.html" data-i18n="menu.books"></a></li>';
+    var newBook = '<li><a href="newBook.html" data-i18n="menu.add"></a></li>';
+
+    var merge =
+        '<nav>' +
+            righthide +
+            index +
+            newBook +
+            '</ul>' +
+            slideOut +
+           index +
+           newBook +
+           '</ul>' +
+            button +
+        '</nav>';
+    $('header').append(merge);
+
+    $(".button-collapse").sideNav();
+}
---
+++ booksApp/www/scripts/pages/newBook.js
@@ -0,0 +1,25 @@
+﻿//functions for newbook.html page
+
+"use strict";
+$(document).ready(function () {
+
+    $('#chkReading').on('click', function () {
+        var checked = $("#chkReading").is(':checked');
+
+        if (checked === true) {
+            $('#divActualPage').show();
+        } else {
+            $('#divActualPage').hide();
+        }
+    });
+
+    $('#FormNewBook').submit(function (e) {
+        alert('it´s come');
+        if ($('#txtName').val() === "") {
+            //$('#lblNameError').attr("data-i18n", "general.fill");
+        }
+
+        e.preventDefault();
+        //alert('works');
+    });
+});
